14ba8fff2310bd67e483247529d7df3e80ff1ed1
Ville Marttinen <ville.marttinen@itvelhot.local>
2022-03-31 10:41:11 +0300

    php

--- backend/registerNewUser.php
+++ backend/registerNewUser.php
@@ -1,5 +1,19 @@
 <?php
+// tarkistuksia
+if (!isset($_POST['username']) || !isset($_POST['password'])){
+    echo 'tapahtui virhe, ei post-dataa saatavilla';
+    die();
+}
 
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+include_once 'pdo-connect.php';
+
+// Luodaan pdo-statement
+$stmt = $conn ->prepare("INSERT INTO user (username, pwd) VALUES (:username, :pwd);");
+$stmt->bindParam(':username', $username);
+$stmt->bindParam(':pwd', $password);
+$stmt_<execute();
 
 echo 'tallennettu';
